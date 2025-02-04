ca8bf6177f15b78d6ab488935631c6d68ec3d838
Ulises Rey <ulises.rey@univie.ac.at>
2025-02-04 17:34:00 +0100

    added subheader

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ st.title("🔍 Web Scraper App")
 
 # User input for URL
 url = st.text_input("Enter a website URL:")
+#url = "https://www.ara.cat/economia/mercats/xina-respon-als-aranzels-trump-taxes-d-10-15_1_5274974.html"
 
 if st.button("Scrape"):
     if url:
@@ -13,25 +14,32 @@ if st.button("Scrape"):
             headers = {"User-Agent": "Mozilla/5.0"}
             page = requests.get(url)
             soup = BeautifulSoup(page.content, 'html.parser')
-            title = soup.find('div', class_='title')
-            subtitle = soup.find('div', class_='subtitle')
+            title = soup.find('h1')
+            subtitle = soup.find('h2')
+            # soup.find('div', class_='subtitle')
+    
             #find the text under div class ara-body
             text = soup.find('div', class_='ara-body')  
+            if title:
+                st.title(title.text)
+            
+            else:
+                st.warning("No title found!")
+
+            if subtitle:
+                st.header(subtitle.text)        
+            else:
+                st.warning("No subtitle found!")
+
             # Extract all paragraphs within the main content
             if text:
                 paragraphs = text.find_all("p")  # Find all <p> tags
                 text = "\n\n".join(p.get_text(strip=True) for p in paragraphs)  # Combine and clean text
                 # print(article_content)
-                st.subheader("Scraped Text:")
-                st.text_area("", text, height=300)
+                st.text(text)
             else:
                 st.warning("No text found!")
 
-            if title:
-                st.subheader("Title:")
-                st.write(title.text)
-            else:
-                st.warning("No title found!")
         except Exception as e:
             st.error(f"Error: {str(e)}")
     else:
